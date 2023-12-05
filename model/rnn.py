e8fe13eda0e1eb01dfd03717d613afa00164d94e
3090 <thisyzg@outlook.com>
2023-12-05 08:26:51 +0000

    What's New:  - Add new RNN model.

--- model/rnn.py
+++ model/rnn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class RNNSimple(nn.Module):
@@ -43,3 +44,62 @@ class RNNClassifier(torch.nn.Module):
         x = self.embedding(x)
         x, h = self.rnn(x)
         return self.fc(x.mean(dim=1))
+
+
+class EncoderRNN(nn.Module):
+    """
+    This is a simple RNN model with one hidden layer, with embedding layer, for seq2seq model.
+    The EncoderRNN is used to encode the input sequence.
+    """
+    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, x):
+        embedded = self.dropout(self.embedding(x))
+        output, hidden = self.gru(embedded)
+        return output, hidden
+
+
+class DecoderRNN(nn.Module):
+    """
+    The DecoderRNN is used to decode the encoded sequence.
+    """
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None, max_length=10, device="cuda", sos_token=0):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=device).fill_(sos_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+
+        for i in range(max_length):
+            decoder_output, decoder_hidden = self.forward_step(decoder_input, decoder_hidden)
+            decoder_outputs.append(decoder_output)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1)  # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        return decoder_outputs, decoder_hidden, None  # We return `None` for consistency in the training loop
+
+    def forward_step(self, x, hidden):
+        output = self.embedding(x)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.out(output)
+        return output, hidden
